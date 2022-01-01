bfa0376a1786532fdcf851b79fb0ddf12a286108
barisdemirkap <barisdemirkap@gmail.com>
2022-01-02 02:58:06 +0300

    methods of datetime initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace methodsofdatetime
+{
+    class Program
+{
+    static void Main(string[] args)
+    {  
+        Console.WriteLine(DateTime.Now) ;
+        Console.WriteLine(DateTime.Now.Date) ;
+        Console.WriteLine(DateTime.Now.Day) ;
+        Console.WriteLine(DateTime.Now.Month) ;
+        Console.WriteLine(DateTime.Now.Year) ;
+        Console.WriteLine(DateTime.Now.Minute) ;
+        Console.WriteLine(DateTime.Now.Second) ;
+        Console.WriteLine(DateTime.Now.DayOfWeek) ;
+        Console.WriteLine(DateTime.Now.DayOfYear) ;
+        Console.WriteLine(DateTime.Now.ToLongDateString()) ;
+        Console.WriteLine(DateTime.Now.ToShortDateString()) ;
+        Console.WriteLine(DateTime.Now.ToLongTimeString()) ;
+        Console.WriteLine(DateTime.Now.ToShortTimeString()) ;
+        Console.WriteLine(DateTime.Now.AddHours(3)) ;
+        Console.WriteLine(DateTime.Now.AddDays (2)) ;
+        Console.WriteLine(DateTime.Now.AddMonths(5)) ;
+        Console.WriteLine(DateTime.Now.AddSeconds(30)) ;
+        Console.WriteLine(DateTime.Now.AddYears(10)) ;
+        Console.WriteLine(DateTime.Now.AddMilliseconds(50)) ;
+        //Datetime Format
+
+        Console.WriteLine(DateTime.Now.ToString("dd")); //24
+        Console.WriteLine(DateTime.Now.ToString("ddd")); //24
+        Console.WriteLine(DateTime.Now.ToString("dddd")); // Sat
+        Console.WriteLine(DateTime.Now.ToString("MM")); // 04
+        Console.WriteLine(DateTime.Now.ToString("MMM")); // Apr     
+        Console.WriteLine(DateTime.Now.ToString("MMMM")); // April
+        Console.WriteLine(DateTime.Now.ToString("yy")); // 21
+        Console.WriteLine(DateTime.Now.ToString("yyyy")); // 2021   
+
+
+        Console.WriteLine("**** Math Kutuphanesi*******"); 
+        //Math Dictionary
+        Console.WriteLine(Math.Abs(-25)); // 25
+        Console.WriteLine(Math.Sin(10)); 
+        Console.WriteLine(Math.Cos(10)); 
+        Console.WriteLine(Math.Tan(10)); 
+        Console.WriteLine(Math.Ceiling(22.3)); // 23
+        Console.WriteLine(Math.Round(22.3)); // 22
+        Console.WriteLine(Math.Round(22.7)); // 23
+        Console.WriteLine(Math.Floor(22.7)); // 22
+        Console.WriteLine(Math.Max(2,6)); // 22
+        Console.WriteLine(Math.Min(2,6)); // 22
+        Console.WriteLine(Math.Pow(3,4)); // 3^4
+        Console.WriteLine(Math.Sqrt(9)); // 3
+        Console.WriteLine(Math.Log(9)); // 9 un e tabanindaki logaritmik karsiligini verir.
+        Console.WriteLine(Math.Exp(3)); // e uzeri 3u verir
+        Console.WriteLine(Math.Log10(10)); // 10 sayisinin logaritma 10 tabanindaki karsiligi
+
+
+
+
+      
+    
+    }
+}
+}
